da913d174877f55034ae5b44f72d35e39ac9e192
andfernando-code <148954130+andfernando-code@users.noreply.github.com>
2024-01-22 02:18:46 +0530

    Update ProductDAOImpl.java

--- Greenmart-App/src/main/java/com/DAO/ProductDAOImpl.java
+++ Greenmart-App/src/main/java/com/DAO/ProductDAOImpl.java
@@ -75,16 +75,210 @@ public class ProductDAOImpl implements ProductDAO{
 		return list;
 	}
 
+ 
+@Override
+public ProductDtls getProductById(int id) {
+	
+	ProductDtls b=null;
+	try {
+		
+		String sql="select * from product_dtls where productId=?";
+		PreparedStatement ps=conn.prepareStatement(sql);
+		ps.setInt(1, id);
+		
+		ResultSet rs=ps.executeQuery();
+		while (rs.next())
+		{
+			b=new ProductDtls();
+			b.setProductId(rs.getInt(1));
+			b.setProductName(rs.getString(2));
+			b.setPrice(rs.getString(3));
+			b.setProductCategory(rs.getString(4));
+			b.setStatus(rs.getString(5));
+			b.setPhotoName(rs.getString(6));
+			b.setEmail(rs.getString(7));
+		}
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	return b;
+	
+}
+
+
 
 @Override
-public List<ProductDtls> getAllBooks() {
+public boolean updateEditProduct(ProductDtls b) {
+	boolean f=false;
+	try {
+		
+		String sql="update product_dtls set productname=?,price=?,status=? where productId=?";
+		PreparedStatement ps=conn.prepareStatement(sql);
+		ps.setString(1, b.getProductName());
+		ps.setString(2, b.getPrice());
+		ps.setString(3, b.getStatus());
+		ps.setInt(4, b.getProductId());
+		
+		int i = ps.executeUpdate();
+		if(i==1)
+		{
+			f=true;
+		}
+		
+	}catch(Exception e){
+		e.printStackTrace();
+	}
 	
-	return null;
+	return f;
 }
+
+
+
+@Override
+public boolean deleteProduct(int id) {
+	boolean f=false;
+	try {
+		String sql="delete from product_dtls where productId=?";
+		PreparedStatement ps=conn.prepareStatement(sql);
+		ps.setInt(1, id);
+		int i=ps.executeUpdate();
+		if(i==1) {
+			f=true;
+		}
+		
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+	
+	return f;
 }
 
+@Override
+public List<ProductDtls> getVegitables() {
+	
+	List<ProductDtls> list=new ArrayList<ProductDtls>();
+	ProductDtls b=null;
+	try {
+		String sql="select * from product_dtls where productCategory=? and status=? order by productId DESC";
+		PreparedStatement ps= conn.prepareStatement(sql);
+		ps.setString(1, "New");
+		ps.setString(2, "Active");
+		ResultSet rs=ps.executeQuery();
+		int i=1;
+		while(rs.next() && i<=4)
+		{
+			b=new ProductDtls();
+			b.setProductId(rs.getInt(1));
+			b.setProductName(rs.getString(2));
+			b.setPrice(rs.getString(3));
+			b.setProductCategory(rs.getString(4));
+			b.setStatus(rs.getString(5));
+			b.setPhotoName(rs.getString(6));
+			b.setEmail(rs.getString(7));
+			list.add(b);
+			i++;
+		}
+		
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	
+	return list;
+}
 
+@Override
+public List<ProductDtls> getFruits() {
+	List<ProductDtls> list=new ArrayList<ProductDtls>();
+	ProductDtls b=null;
+	try {
+		String sql="select * from product_dtls where status=? order by productId DESC";
+		PreparedStatement ps= conn.prepareStatement(sql);
+		ps.setString(1, "Active");
+		ResultSet rs=ps.executeQuery();
+		int i=1;
+		while(rs.next() && i<=4)
+		{
+			b=new ProductDtls();
+			b.setProductId(rs.getInt(1));
+			b.setProductName(rs.getString(2));
+			b.setPrice(rs.getString(3));
+			b.setProductCategory(rs.getString(4));
+			b.setStatus(rs.getString(5));
+			b.setPhotoName(rs.getString(6));
+			b.setEmail(rs.getString(7));
+			list.add(b);
+			i++;
+		}
+		
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	
+	return list;
+}
 
+@Override
+public List<ProductDtls> getAllVegitables() {
+	List<ProductDtls> list=new ArrayList<ProductDtls>();
+	ProductDtls b=null;
+	try {
+		String sql="select * from product_dtls where productCategory=? and status=? order by productId DESC";
+		PreparedStatement ps= conn.prepareStatement(sql);
+		ps.setString(1, "New");
+		ps.setString(2, "Active");
+		ResultSet rs=ps.executeQuery();
+		while(rs.next())
+		{
+			b=new ProductDtls();
+			b.setProductId(rs.getInt(1));
+			b.setProductName(rs.getString(2));
+			b.setPrice(rs.getString(3));
+			b.setProductCategory(rs.getString(4));
+			b.setStatus(rs.getString(5));
+			b.setPhotoName(rs.getString(6));
+			b.setEmail(rs.getString(7));
+			list.add(b);
+		}
+		
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	
+	return list;
+}
 
+@Override
+public List<ProductDtls> getAllFruits() {
+	List<ProductDtls> list=new ArrayList<ProductDtls>();
+	ProductDtls b=null;
+	try {
+		String sql="select * from product_dtls where status=? order by productId DESC";
+		PreparedStatement ps= conn.prepareStatement(sql);
+		ps.setString(1, "Active");
+		ResultSet rs=ps.executeQuery();
+		while(rs.next())
+		{
+			b=new ProductDtls();
+			b.setProductId(rs.getInt(1));
+			b.setProductName(rs.getString(2));
+			b.setPrice(rs.getString(3));
+			b.setProductCategory(rs.getString(4));
+			b.setStatus(rs.getString(5));
+			b.setPhotoName(rs.getString(6));
+			b.setEmail(rs.getString(7));
+			list.add(b);
+		}
+		
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	
+	return list;
+}
 
-
+}
