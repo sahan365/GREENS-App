61cb71ef2db9040eef89363c048efecf19db283d
andfernando-code <148954130+andfernando-code@users.noreply.github.com>
2024-01-22 02:19:25 +0530

    Update ProductDAO.java

--- Greenmart-App/src/main/java/com/DAO/ProductDAO.java
+++ Greenmart-App/src/main/java/com/DAO/ProductDAO.java
@@ -8,5 +8,20 @@ public interface ProductDAO {
 	
 	public boolean addProduct(ProductDtls b);
 	
-	public List<ProductDtls> getAllBooks();
+	public List<ProductDtls> getAllProduct();
+	
+	public ProductDtls getProductById(int id);
+	
+	public boolean updateEditProduct(ProductDtls b);
+	
+	public boolean deleteProduct(int id);
+	
+	public List<ProductDtls> getVegitables();
+	
+	public List<ProductDtls> getFruits();
+	
+	public List<ProductDtls> getAllVegitables();
+	
+	public List<ProductDtls> getAllFruits();
+	
 }
